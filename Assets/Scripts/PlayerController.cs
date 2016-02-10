30b253d30a922d097d45271acf91949fd088c8f0
Heesoo Home <Heesoo@Heesoo-PC>
2016-02-09 22:26:57 -0800

    Player Movement Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,30 +3,40 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 5f;
+    public float speed = 3f;
     private Rigidbody2D playerBody;
     private Transform player;
-    
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKeyDown(KeyCode.A))
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector2(-1 * speed * Time.deltaTime, 0);
+            transform.position += new Vector3(-1 * Time.deltaTime * speed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1 * Time.deltaTime * speed, 0, 0);
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1 * Time.deltaTime * speed, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position = new Vector2(1 * speed * Time.deltaTime, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-
-        }
+            transform.position += new Vector3(1 * Time.deltaTime * speed, 0, 0);
+        }*/
     }
 
     //Daniel Choi is a demigod and deminoob
