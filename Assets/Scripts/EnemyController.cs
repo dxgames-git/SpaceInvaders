76fd338bcf9390bd59e43330e3c0a4ca34e16156
Daniel Choi <daniel.won.choi@gmail.com>
2016-02-09 22:57:50 -0800

    Alien Script Implemented

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class EnemyController : MonoBehaviour {
 
+    private Transform Alien;
+    private float speed = 1f;
+ 
 	// Use this for initialization
 	void Start () {
-	//Heesoo is a Newb
-	}
+        //Heesoo is a Newb   
+    }
+}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+    transform.position = new Vector3(-1 * Time.deltaTime * speed, 1 * Time.deltaTime * speed, 0);
+}
 }
